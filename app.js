dac92b1d54d9cbb6901f34a8e2408fa2d3783978
JUSTFIDEL <fidenki1429@gmail.com>
2023-03-27 13:45:58 +0100

    .env, git-ignore added

--- app.js
+++ app.js
@@ -1,76 +1,68 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
 const https = require('https')
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
-
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/sign-up.html')
+	res.sendFile(__dirname + '/sign-up.html')
 })
 
-
 app.post('/', (req, res) => {
+	let firstName = req.body.fName
+	let lastName = req.body.lName
+	let email = req.body.email
 
-    let firstName = req.body.fName;
-    let lastName = req.body.lName;
-    let email = req.body.email;
+	const data = {
+		members: [
+			{
+				email_address: email,
+				status: 'subscribed',
+				merge_fields: {
+					FNAME: firstName,
+					LNAME: lastName,
+				},
+			},
+		],
+	}
 
-    const data = {
-        members: [
-            {
-                email_address: email,
-                status: 'subscribed',
-                merge_fields: {
-                    FNAME: firstName,
-                    LNAME: lastName
-                }
-            }
-        ]
-    }
+	const jsonData = JSON.stringify(data)
 
-    const jsonData = JSON.stringify(data)
+	const url = 'https://us17.api.mailchimp.com/3.0/lists/' + process.env.SERVER
 
-    const url = 'https://us17.api.mailchimp.com/3.0/lists/f96cbb5cb6'
+	const options = {
+		method: 'POST',
+		auth: 'justfidel:' + process.env.API_KEY,
+	}
+	const request = https.request(url, options, response => {
+		if (response.statusCode === 200) {
+			res.sendFile(__dirname + '/success.html')
+		} else {
+			res.sendFile(__dirname + '/failure.html')
+		}
 
-    const options = {
-        method: 'POST',
-        auth: 'justfidel:9584ac1b77ce3ea61d72bb21f91024e1-us17'
+		response.on('data', data => {
+			console.log(JSON.parse(data))
+		})
+	})
 
-    }
-    const request = https.request(url, options, (response) => {
-        
-        if (response.statusCode === 200) {
-            res.sendFile(__dirname + '/success.html')
-        } else {
-            res.sendFile(__dirname + '/failure.html')
-        }
-        
-        response.on('data', (data) => {
-            console.log(JSON.parse(data));
-        })
-    })
-
-    request.write(jsonData);
-    request.end();
+	request.write(jsonData)
+	request.end()
 })
 
 app.post('/success', (req, res) => {
-    res.redirect('/')
+	res.redirect('/')
 })
 
 app.post('/failure', (req, res) => {
-    res.redirect('/')
+	res.redirect('/')
 })
-
-
 
 app.listen(process.env.POST || 3000, (req, res) => {
-    console.log('Server starting at Port 3000');
+	console.log('Server starting at Port 3000')
 })
-
-
